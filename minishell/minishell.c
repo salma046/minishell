9a7733e0fed2b48e776e1df82893738bde1d6aec
salaoui <salaoui@c4r8p3.1337.ma>
2024-09-27 14:30:14 +0100

    fixed dollar not fixed

---
+++ minishell/minishell.c
@@ -0,0 +1,199 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: salaoui <salaoui@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/30 09:49:12 by salaoui           #+#    #+#             */
+/*   Updated: 2024/09/26 12:20:16 by salaoui          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_minishell	g_minishell;
+
+// search_errors(void)
+// in here first check if the first token is a pipe
+// check if the last token is a pipe | all_redires
+// check if there are two followed tokens and both are pipes
+
+void	ft_redi_add_back(t_redir **redirections, t_redir *new_redir)
+{
+	t_redir *arrs;
+
+	arrs = *redirections;
+	if (!redirections || !new_redir)
+		return ;
+	if (*redirections == NULL)
+	{
+		*redirections = new_redir;
+		return ;
+	}
+	while (arrs->next != NULL)
+		arrs = arrs->next;
+	arrs->next = new_redir;
+}
+
+void	fill_redi(enum e_token_type token_t, char *red_file, t_redir **redirections)
+{
+	t_redir *new_redir;
+
+	new_redir = malloc(sizeof(t_redir));
+	if(!new_redir)
+		return ;
+	new_redir->red_type = token_t;
+	new_redir->file = ft_strdup(red_file);
+	new_redir->next = NULL;
+	ft_redi_add_back(redirections, new_redir);
+}
+
+void	ft_node_add_back(t_node **node_list, t_node *new_node)
+{
+	t_node *arrs;
+
+	arrs = *node_list;
+	if (!node_list || !new_node)
+		return ;
+	if (*node_list == NULL)
+	{
+		*node_list = new_node;
+		return ;
+	}
+	while (arrs->next_node != NULL)
+		arrs = arrs->next_node;
+	arrs->next_node = new_node;
+	new_node->prev_node = arrs;
+}
+
+int cmd_count(t_token *tokens)
+{
+	int i;
+
+	i = 0;
+	while (tokens && tokens->data_type != PIPE)
+	{
+		if (tokens->data_type == OUT_REDIR || tokens->data_type == APPEND ||
+            tokens->data_type == INP_REDIR || tokens->data_type == HER_DOC)
+			tokens = tokens->next_token;
+		else
+		{
+			i++;
+		}
+		tokens = tokens->next_token;
+	}
+	return (i);
+}
+
+void	fill_node(t_token *temp_tokens, t_node **node_list)
+{
+	t_node *node;
+	t_redir *redirections;
+	t_token *tokens;
+	int		cmd_idx;
+	int		i;
+
+	i = 0;
+	tokens = temp_tokens;
+	redirections = NULL;
+	node = malloc(sizeof(t_node));
+    if (!node)
+		return ;
+	cmd_idx = cmd_count(temp_tokens);
+	node->cmd = malloc(sizeof(char *) * (cmd_idx + 1));
+	while (tokens && tokens->data_type != PIPE)
+	{
+		if (tokens->data_type == OUT_REDIR || tokens->data_type == APPEND ||
+            tokens->data_type == INP_REDIR || tokens->data_type == HER_DOC)
+		{
+			fill_redi(tokens->data_type, tokens->next_token->data, &redirections);	
+			tokens = tokens->next_token;
+		}
+		else
+			node->cmd[i++] = ft_strdup(tokens->data);
+		tokens = tokens->next_token;
+	}
+	node->redir = redirections;
+	node->cmd[i] = NULL;
+	node->next_node = NULL;
+	ft_node_add_back(node_list, node);
+}
+
+void	fill_commands(t_node **node_list, t_token *tokens, int num_cmds)
+{
+    int cmd_idx = 0;
+    int arg_idx = 0;
+
+    while (tokens) {
+		fill_node(tokens, node_list);
+		while (tokens && tokens->data_type != PIPE)
+			tokens = tokens->next_token;
+        if (tokens && tokens->data_type == PIPE)
+			tokens = tokens->next_token;
+    }
+}
+
+t_node	*mk_nodes(t_token *tokens)
+{
+	t_node	*nodes;
+	int		cmd_count;
+	int		i;
+
+	nodes = NULL;
+	i = 0;
+	// count pipes in one command
+	cmd_count = count_pipe(tokens);
+	
+	fill_commands(&nodes, tokens, cmd_count);
+	return (nodes);
+}
+
+int	main(int ac, char *av[], char **env)
+{
+	g_minishell.envirement = env;
+	int i;
+	int j;
+
+	while (1)
+	{
+		// doing signals -sigint & -sigquit
+		g_minishell.command = readline("Minishell~$ ");
+		if (!g_minishell.command)
+		{
+			printf("Quiting minishell!\n");
+			exit(1);
+		}
+		add_history(g_minishell.command);
+		g_minishell.tokens = ft_tokenize(g_minishell);
+		if (!g_minishell.tokens)
+			continue ;
+		g_minishell.tokens = rmp_dollar(g_minishell.tokens); // in progress // Done
+		g_minishell.tokens = rm_qotes(g_minishell.tokens);
+		g_minishell.tokens = parsing(g_minishell);
+		g_minishell.nodes = mk_nodes(g_minishell.tokens);
+		while (g_minishell.nodes)
+		{
+			printf("----------------------------------------------------------\n");
+			j = 0;
+			i = 0;
+			while(g_minishell.nodes->cmd[j])
+			{
+				printf("the node \033[32m%d\033[0m cmds n* %d is :\033[32m %s\033[0m\n",
+				i, j, g_minishell.nodes->cmd[j]);
+				j++;
+			}
+			while(g_minishell.nodes->redir)
+			{
+				printf("the redir file name is: %s\n",
+					g_minishell.nodes->redir->file);
+				printf("the redir type is: %d\n",
+					g_minishell.nodes->redir->red_type);
+				g_minishell.nodes->redir = g_minishell.nodes->redir->next;
+			}
+			printf("----------------------------------------------------------\n");
+			g_minishell.nodes = g_minishell.nodes->next_node;
+			i++;
+		}
+	}
+}
---
+++ minishell/minishell.h
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: salaoui <salaoui@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/30 09:49:06 by salaoui           #+#    #+#             */
+/*   Updated: 2024/09/26 13:41:42 by salaoui          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef enum e_token_type
+{
+	WORD,
+	PIPE,
+	HER_DOC,
+	OUT_REDIR,
+	APPEND,
+	INP_REDIR
+}					token_type;
+
+typedef struct s_token
+{
+	char			*data;
+	token_type		data_type;
+	struct s_token	*next_token;
+	struct s_token	*prev_token;
+}	t_token;
+
+typedef struct s_redir
+{
+	token_type		red_type;
+	char			*file;
+	struct s_redir	*next;
+}	t_redir;
+
+typedef struct s_node
+{
+	char				**cmd;
+	t_redir				*redir;
+	struct s_node	*next_node;
+	struct s_node	*prev_node;
+}	t_node;
+
+typedef struct s_minishell
+{
+	char	**envirement;
+	char	*command;
+	t_token	*tokens;
+	t_node	*nodes;
+}					t_minishell;
+
+extern t_minishell	minishell;
+
+t_token *ft_tokenize(t_minishell g_minishell);
+int		ft_strncmp(char *s1, const char *s2, size_t n);
+int		 is_space(char *line);
+void	ft_lstadd_back(t_token **lst, t_token *new);
+char	*get_word(char *str, int i);
+void	ft_put_token(char **line, enum e_token_type token_t, t_token **tokens_list);
+int		ft_put_word_token(char **line, enum e_token_type token_t, t_token **tokens_list);
+size_t	ft_strlen(const char *str); 	
+t_token	*rm_qotes(t_token *tokens);
+t_token	*parsing(t_minishell g_minishell);
+t_token	*rmp_dollar(t_token *tokens);
+int		count_pipe(t_token *tokens);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *src);
+int		is_not_alpanum(char c);
+
+// minitest in the end of prog
+// cat lksdj < dalkf;> sdkfj<<sdf >>dslfj |ksdjj|osdfij|sdkfj+
+# endif
