21835cc3a16f500216333f7cab96819d7c66a283
soko <soko@gmail.com>
2024-10-21 14:55:37 +0100

    o

---
+++ checking_cmd.c
@@ -0,0 +1,18 @@
+#include "minishell.h"
+
+void check_command(t_token data)
+{
+	// t_env *env_list =  NULL;
+	if (!ft_strcmp(data.tokens->data , "echo") && data.tokens->data)
+    	ft_echo(data);
+	if (!ft_strcmp(data.tokens->data , "cd") && data.tokens->data)
+		ft_cd(data);
+	if (!ft_strcmp(data.tokens->data , "pwd") && data.tokens->data)
+		ft_pwd(data);
+	if (!ft_strcmp(data.tokens->data , "env") && data.tokens->data)
+		ft_env(data);
+	if (!ft_strcmp(data.tokens->data , "exit") && data.tokens->data)
+		ft_exit(data);
+	if (!ft_strcmp(data.tokens->data , "unset") && data.tokens->data)
+		ft_unset(env_list,  data);
+}
--- ft_echo.c
+++ ft_echo.c
@@ -1,36 +1,38 @@
 
 #include "minishell.h"
-void ft_print(t_minishell data)
+void ft_print(t_token *tokens)
 {
-    while (data.tokens)
+    t_token tmp_token;
+    tmp_token = *tokens;
+    while (tmp_token)
     {
-        write(1, data.tokens->data, ft_strlen(data.tokens->data));
-        if (data.tokens->next_token != NULL)
+        write(1, tmp_token->data, ft_strlen(tmp_token.data));
+        if (tmp_token.next_token != NULL)
             write(1, " ", 1);
-        data.tokens = data.tokens->next_token;
+        tmp_token = tmp_token.next_token;
     }
 }
 
-void ft_echo(t_minishell data)
+void ft_echo(t_token tokens)
 { 
     printf("         -> \033[0;31m Ana f echo\033[0m <-\n");
-    if (data.tokens->data)
+    if (tokens.data)
     {
-        if (data.tokens->next_token == NULL)
+        if (tokens.next_token == NULL)
         {
             write(1, "\n", 1);
             return;
         }
-        if (!ft_strncmp(data.tokens->next_token->data , "-n",2))
+        if (!ft_strncmp(tokens.next_token->data , "-n",2))
         {
-            data.tokens = data.tokens->next_token->next_token;
-            if (data.tokens == NULL )
+            tokens = tokens.next_token->next_token;
+            if (tokens == NULL )
                 return;
             ft_print(data);
         }
         else 
         {
-            data.tokens = data.tokens->next_token;
+            tokens = tokens.next_token;
             ft_print(data);
             write(1, "\n",1 );
         }
